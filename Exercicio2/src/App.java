59832332045d1c24488d1d40656dd27a40284c33
Lucas Marques Ferreira Souza <lucas.marques05@outlook.com>
2026-09-01 23:56:19 +0000

    Exercicio2

---
+++ Exercicio2/src/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner leitor = new Scanner (System.in);
+        //entrada;
+    
+        System.out.println("Digite a temperatura em graus:");
+        double temperatura = leitor.nextDouble(); 
+
+        //processamento;
+
+        double F = temperatura * 9.0 / 5.0 + 32;
+
+        //saida;
+
+        System.out.println ("Convertendo a temperatura de " + temperatura + "°C para Farenheit o resultado será 50" + F +"°F" );
+    }
+}
